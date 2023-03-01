0f9787a7d1223b22fd2e3eb90df88fc8e1073358
Bruno Buso <bruno.buso@sovos.com>
2023-03-01 00:04:00 -0300

    COMMIT INITIAL IMPERIUM

---
+++ src/main/java/br/com/buscouty/imperium/ImperiumApplication.java
@@ -0,0 +1,13 @@
+package br.com.buscouty.imperium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/buscouty/imperium/ImperiumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buscouty.imperium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
